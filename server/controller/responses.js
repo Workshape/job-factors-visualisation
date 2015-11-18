15d56bb24431c525f3977c5dd27cea19d5b0f49c
Tancredi Trugenberger <tancredi.trugenberger@gmail.com>
2015-11-18 21:12:38 +0000

    Mobile optimisations

--- server/controller/responses.js
+++ server/controller/responses.js
@@ -8,10 +8,26 @@ module.exports = function (req, res, next) {
             fs.readFile(RESPONSES_PATH, 'utf8', function (err, content) {
                 if (err) { return next(err); }
 
-                res.status(200).send({ data: JSON.parse(content) });
+                res.status(200).send({ responses: processData(JSON.parse(content)) });
             });
         } else {
             res.status(404).send('Data not available');
         }
     });
 };
+
+function processData(data) {
+    return data.responses.map(function (response) {
+        return Object.keys(response.answers).map(function (key) {
+            return parseValue(response.answers[key]);
+        });
+    });
+}
+
+function parseValue(val) {
+    if (parseInt(val, 10) + '' === val + '') {
+        return parseInt(val, 10);
+    }
+
+    return val;
+}
